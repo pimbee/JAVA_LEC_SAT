9d9011045c0f1dc91a883ef208059ded678b0eda
PIMBEE <pimbeelovenaver.com>
2022-06-25 16:24:37 +0900

    commit

--- Day01/MySecondClass.java
+++ Day01/MySecondClass.java
@@ -3,5 +3,6 @@ public class MySecondClass {//자바 클래스의 시작
 	/*클래스블록*/
 	public static void main(String[] args) {//메서드 시작
 		/*메서드블록*/
+			System.out.println("git commit 후 변경해 보기");
 	}//메서드꿑
 }//자바 클래스의 끝

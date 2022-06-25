2e1843f35f861e689e70037402964661cddb32cf
PIMBEE <pimbeelovenaver.com>
2022-06-25 16:20:09 +0900

    나의 첫번째 git commit

---
+++ Day01/MySecondClass.java
@@ -0,0 +1,7 @@
+
+public class MySecondClass {//자바 클래스의 시작
+	/*클래스블록*/
+	public static void main(String[] args) {//메서드 시작
+		/*메서드블록*/
+	}//메서드꿑
+}//자바 클래스의 끝
---
+++ Day01/MyfirstClass.java
@@ -0,0 +1,8 @@
+
+public class MyfirstClass {
+
+		public static void main(String[] args) {
+			System.out.println("My First Class");
+			System.out.println("안녕 자바");
+		}
+}
